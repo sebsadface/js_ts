04248b7b9fc7f819f694ad2aa2926d360cd1289a
Sebastian Liu <sebsadface@gmail.com>
2022-07-05 18:44:27 -0700

    trivia update'

---
+++ quiz-app/src/App.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import QuestionCard from './components/QuestionCard';
+
+
+const App = () => {
+  const [Loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {
+
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+
+  }
+
+  const nextQuestion = () => {
+
+
+  }
+
+  return (
+    <div className="App">
+      <h1>TRIVIA</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'> Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard
+        questionNum={number + 1}
+
+
+      />
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+}
+
+export default App;
+
