21a94ad446821a57481a761938900974f140f7fd
Sebastian Liu <sebsadface@gmail.com>
2022-07-05 19:16:58 -0700

    update api.ts

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
+import { fetchQuizQuestions } from './api'
+import { Difficulty } from './api'
 import QuestionCard from './components/QuestionCard';
 
+const TOTAL_QUESTIONS = 20;
 
 const App = () => {
   const [Loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [number, setNumber] = useState(0);
-  const [userAnswer, setUserAnswer] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.HARD))
+
 
   const startTrivia = async () => {
 
@@ -33,11 +39,14 @@ const App = () => {
       </button>
       <p className='score'> Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNum={number + 1}
-
-
-      />
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
       <button className='next' onClick={nextQuestion}>
         Next Question
       </button>
