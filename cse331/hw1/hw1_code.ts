81847437f0c86261ca28b4280a5c942743d8d4e7
Sebastian Liu <sebsadface@gmail.com>
2023-04-02 20:46:30 -0700

    q2 update

---
+++ cse331/hw1/hw1_code.ts
@@ -0,0 +1,6 @@
+ function f(a : number, b : boolean) : [number, boolean] {
+                const num : number = 2 * a;
+                const bool : boolean = true;
+
+                return [num, bool];
+            }
